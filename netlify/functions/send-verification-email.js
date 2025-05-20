f6871e4ce8bd07afd577758c948cd3c98a10d55b
AbdelrhmanH <16557494+AbdelrhmanH@users.noreply.github.com>
2025-05-20 18:45:41 +0300

    Update send-verification-email.js

--- netlify/functions/send-verification-email.js
+++ netlify/functions/send-verification-email.js
@@ -11,7 +11,7 @@ const sendgridMail = require('@sendgrid/mail');
       }),
     });
   }
-
+console.log("API KEY:", process.env.SENDGRID_API_KEY);
   const db = admin.firestore();
 
   exports.handler = async (event, context) => {
@@ -75,7 +75,7 @@ const sendgridMail = require('@sendgrid/mail');
       console.error('Error:', error);
       return {
         statusCode: 500,
-        body: JSON.stringify({ error: 'Failed to send email' }),
+        body: JSON.stringify({ error: 'Failed to send email' process.env.SENDGRID_API_KEY }),
       };
     }
   };
